7ae483061fcf6f6df5d99e59df9121efe831e9d1
Michael (Doc) Norton <docondev@gmail.com>
2012-12-23 10:56:35 -0500

    Reads from passed-in json file

--- lib/cfd_data.rb
+++ lib/cfd_data.rb
@@ -1,8 +1,12 @@
 class CFDData
+  require 'json'
   def initialize(file_name='cfd.json')
+    @file_name = file_name
+    @cfd = {}
+    @cfd = JSON.parse(IO.read(@file_name)) if File.exists?(@file_name)
   end
 
   def record_count
-    0
+    @cfd.count
   end
 end
--- spec/cfd_data_spec.rb
+++ spec/cfd_data_spec.rb
@@ -5,12 +5,26 @@ require 'cfd_data'
 # Write data to external file
 
 describe CFDData do
+  subject 
+    CFDData new(file_name)
+
   describe 'read data from file' do
     context 'missing file' do
       it 'should have 0 records' do
+        let(:file_name) { 'bogus_file.json' }
+        its(:count) { should 0}
+
         cfd = CFDData.new('bogus_file.json')
         cfd.record_count.should == 0
       end
     end
+
+    context 'populated data file' do
+      it 'should read 4 records from sample file' do
+        cfd = CFDData.new('sample.json')
+        cfd.record_count.should == 4
+      end
+    end
+
   end
 end
