43ebcdd3fc26575ba5b2238e0e647c0a21944787
Michael (Doc) Norton <docondev@gmail.com>
2012-12-23 20:10:30 -0500

    Sets up basic node structure

---
+++ personalize.js
@@ -0,0 +1,10 @@
+var router = require("./node_router");
+var server = require("./node_server");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
