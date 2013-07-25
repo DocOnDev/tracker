9942bcc8aa1185d10bac9a7f8a6aa33b27888a28
Michael (Doc) Norton <michael@docondev.com>
2013-07-25 09:21:56 -0500

    Reconstruct runner so it can recover better

--- runner.rb
+++ runner.rb
@@ -9,80 +9,45 @@ def report_stories(stories, label)
   p "#{label}: #{stories.count} / #{stories.points}"
 end
 
-def run_stats
+def load_projects
+  project_details = Hash.new
+  project_details["Pretty Face"] = {"tracker" => 727223, "labels" => nil, "database" => "pretty_face"}
+  project_details["DevSpect"] = {"tracker" => 707539, "labels" => nil, "database" => "devspect"}
+  project_details["Personalize"] = {"tracker" => 52897, "labels" => nil, "database" => "personalize"}
+  project_details["MegaMind"] = {"tracker" => 313781, "labels" => nil, "database" => "megamind"}
+  project_details["Pull"] = {"tracker" => 614999, "labels" => nil, "database" => "pull"}
+  project_details["Pull/I-Tier"] = {"tracker" => 614999, "labels" => ["i-tier"], "database" => "pull_itier"}
+  project_details["Breadcrumb"] = {"tracker" => 567623, "labels" => nil, "database" => "breadcrumb"}
+  project_details["Humor"] = {"tracker" => 578505, "labels" => nil, "database" => "humor"}
+  # project_details["Reserve"] = {"tracker" => 725097, "labels" => nil, "database" => "reserve"}
+  project_details["Deal Estate"] = {"tracker" => 126097, "labels" => nil, "database" => "deal_estate"}
+  project_details["Deal With It"] = {"tracker" => 358255, "labels" => nil, "database" => "deal_with_it"}
+  project_details["Deal Wizard"] = {"tracker" => 547815, "labels" => nil, "database" => "deal_wizard"}
+  project_details["Quantum Lead"] = {"tracker" => 584807, "labels" => nil, "database" => "quantum_lead"}
+  project_details["Coffee"] = {"tracker" => 500037, "labels" => nil, "database" => "coffee"}
+  project_details["Optimize Platform"] = {"tracker" => 386283, "labels" => nil, "database" => "optimize_platform"}
+  project_details["Communication Engineering"] = {"tracker" => 273627, "labels" => nil, "database" => "comm_eng"}
+  project_details
+end
+
+def run
+  @projects = load_projects
   p "===================================="
   p "CFD data for #{Time.new.localtime}"
   p "===================================="
-  p "Pretty Face"
-  hit_live 727223, nil, ["Doc Norton", "Jeff Morgan", "Joel Byler", "Steve Jackson", "Ben Woznicki"]
-  save_data "pretty_face", nil
-  p "===================================="
-  p "DevSpect"
-  hit_live 707539, nil, ["Doc Norton"]
-  save_data "devspect", nil
-  p "===================================="
-  #p "Personalize/HeartX"
-  #hit_live 52897, ["heartx", "my profile", "personal_collections"], ["Kofi Appiah", "Dan Gilbert", "Jeff Long", "Kevin Tao", "Jason Felice"]
-  #save_data "personalization", ["heartx", "my profile", "personal_collections"]
-  #p "===================================="
-  p "Personalize"
-  hit_live 52897, nil, ["Kofi Appiah", "Dan Gilbert", "Jeff Long", "Kevin Tao", "Jason Felice"]
-  save_data "personalize", nil
-  p "===================================="
-  p "MegaMind"
-  hit_live 313781, nil, ["Pete Prokopowicz", "Varun Loiwal"]
-  save_data "megamind", nil
-  p "===================================="
-  p "Pull"
-  hit_live 614999, nil, ["Darby Frey", "Ruslan Gilfanov", "Ben Haley", "Ryan Kinderman", "Sean Massa", "Keith Norman", "Ben Reinhart", "Sean White"]
-  save_data "pull", nil
-  p "===================================="
-  p "Pull/I-Tier"
-  hit_live 614999, ["i-tier"], ["Ben Haley", "Ryan Kinderman", "Sean Massa", "Keith Norman", "Ben Reinhart"]
-  save_data "pull_itier", ["i-tier"]
-  p "===================================="
-  p "Breadcrumb"
-  hit_live 567623, nil, ["Bogdan Filioreanu", "Ciprian Tarta", "Jason Crawford", "Bogdan Filioreanu", "Mahmud Din", "Zeke Huang", "Tim Mun", "Ben Bernard", "Andy Andrei Hurjui", "Andrew Miner", "Blake Scholl"]
-  save_data "breadcrumb", nil
-  p "===================================="
-  p "Humor"
-  hit_live 578505, nil, ["Ian O'Dea"]
-  save_data "humor", nil
-  p "===================================="
-  p "Reserve"
-  hit_live 725097, nil, ["Scott Rogers", "Stephen Johnston", "Dave Kong", "Par Trivedi", "Gerardo Diaz"]
-  save_data "reserve", nil
-  p "===================================="
-  p "Deal Estate"
-  hit_live 126097, nil, ["Geoff Massanek", "Eric Meyer", "Dean Marano", "Carl Thuringer", "Sulabh Jain", "Syed Hussain", "Michael Hines", "Lauri Reeves", "Wai Feman", "Vartika Singh"]
-  save_data "deal_estate", nil
-  p "===================================="
-  p "Deal With It"
-  hit_live 358255, nil, ["Dave Moore", "Patrick Gombert", "Anthony Caliendo", "Blake Smith"]
-  save_data "deal_with_it", nil
-  p "===================================="
-  p "Local Deal Features"
-  hit_live 639881, nil, ["Lucas Willett", "Turner King", "Marcus Sacco", "Richie Vos", "Derrick Spell"]
-  save_data "local_deal", nil
-  p "===================================="
-  # p "Deal Wizard"
-  # hit_live 547815, nil, ["Ashish Dixit", "Stephen Wooten", "Dmitriy Pashkin", "Kavita Kochar", "Tom Calloway"]
-  # save_data "deal_wizard", nil
-  # p "===================================="
-  p "Quantum Lead"
-  hit_live 584807, nil, ["Mike Busch", "Kris Urie", "Bob Beaty", "Darek Pasciak", "Matt", "Gary Fredericks", "Denny Abraham", "Jeremy Neander"]
-  save_data "quantum_lead", nil
-  p "===================================="
-  p "Coffee"
-  hit_live 500037, nil, ["Sergey Varaksin", "Dmitrii Abramov", "Shelby King"]
-  save_data "coffee", nil
-  p "===================================="
-  p "Optimize Platform"
-  hit_live 386283, nil, ["Dave Moore", "Patrick Gombert", "Anthony Caliendo", "Blake Smith"]
-  save_data "optimize_platform", nil
-  p "===================================="
+  run_stats
 end
 
+
+def run_stats
+  @projects.select{|k,v| !v["complete"]}.each do |name, project|
+    p name
+    hit_live project["tracker"], project["labels"]
+    save_data project["database"], project["labels"]
+    project["complete"] = true
+    p "===================================="
+  end
+end
 
 def save_data project_name, labels
   cfd = CFDData.new(CFDCouchIO.new(project_name))
@@ -95,7 +60,7 @@ def save_data project_name, labels
   vel.write
 end
 
-def hit_live project_id, label, owners
+def hit_live project_id, label
   @reader = TrackerReader.new
   @reader.project = project_id
   report_stories(@reader.state(:accepted).label(label), "Accepted")
@@ -104,13 +69,8 @@ def hit_live project_id, label, owners
   report_stories(@reader.state(:finished).label(label), "Finished")
   report_stories(@reader.state(:started).label(label), "Started")
   report_stories(@reader.state(:backlog).label(label), "Backlog")
-  report_stories(@reader.type(:bug).label(label), "All Defects")
   report_stories(@reader.iteration(:current).label(label).type(:bug), "Current Defects")
-  report_stories(@reader.type(:Chore).label(label), "Chores")
   report_stories(@reader.iteration(:current).label(label).type(:chore), "Current Chores")
-  owners.each do |owner|
-    report_stories(@reader.state(:wip).owner(owner).label(label), owner)
-  end
   report_stories(@reader.state(:icebox).label(label), "Icebox")
   report_stories(@reader.state(:wip).label(label), "Total WIP")
   report_stories(@reader.iteration(:current).label(label).state(:accepted), "Current Velocity")
