ddaca4b81b9c4041d2ce483df145115b39d1e4e5
Michael (Doc) Norton <docondev@gmail.com>
2013-09-22 10:15:28 -0500

    Focus on Reader. Transformer to come later.

--- lib/transformer.rb
+++ lib/transformer.rb
@@ -1,5 +1,10 @@
+#
+# receives a ruby object (hash) and transforms into DevSpectStories object
+#
+
 class Transformer
   def transform json_data
+    raise "No data received" if json_data.length == 0
     raise "Invalid data source"
   end
 
--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -4,13 +4,16 @@ require 'json'
 describe Transformer do
 
   describe "#transform" do
-    it "raises an error when the json is empty" do
+    it "raises a No data received error when there is no json" do
       transformer = Transformer.new
-      lambda { transformer.transform JSON.parse("[]") }.should raise_error("Invalid data source")
+      lambda { transformer.transform "" }.should raise_error("No data received")
     end
 
-    
-
+    it "returns an empty story collection when the json is empty" do
+      # transformer = Transformer.new
+      # result = transformer.transform JSON.parse("[]")
+      # result.should be_kind_of Stories
+    end
    
   end
 
