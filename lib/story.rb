d85f59452bfd1173d6a701024884d2a563bd9bed
Michael (Doc) Norton <docondev@gmail.com>
2013-09-23 21:45:53 -0500

    Fixes date formatting. Verifies story.

--- lib/story.rb
+++ lib/story.rb
@@ -1,11 +1,12 @@
 class Story
-  attr_accessor :name, :status, :updated, :type, :creator, :created, :size, :url
+  DATE_TIME_FORMAT = '%m/%d/%Y %H:%M:%S'
+  attr_accessor :name, :status, :updated, :type, :creator, :created, :size, :url, :owner
 
   def created_date
-    DateTime.new(@created).strftime('%F %T')
+    DateTime.strptime("#{@created/1000}", "%s").strftime(DATE_TIME_FORMAT)
   end
 
   def updated_date
-    DateTime.new(@updated).strftime('%F %T')
+    DateTime.strptime("#{@updated/1000}", "%s").strftime(DATE_TIME_FORMAT)
   end
 end
--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -19,7 +19,7 @@ describe Transformer do
     context 'with json that is not empty' do
       before(:all) do
         @stories = Transformer.transform(JSON.parse(File.read("features/support/tracker_data.json")))
-        @story = @stories[0]
+        @first_story = @stories[0]
       end
 
       it 'has a story count greater than 0' do
@@ -27,44 +27,53 @@ describe Transformer do
       end
 
       it 'contains a story' do
-        @stories[0].should be_kind_of(Story)
+        @first_story.should be_kind_of(Story)
       end
 
       context 'has the required fields' do
         it 'name' do
-          @story.name.should_not be_empty
+          @first_story.name.should_not be_empty
         end
 
         it 'status' do
-          @story.status.should_not be_empty
+          @first_story.status.should_not be_empty
         end
 
         it 'updated date' do
-          @story.updated_date.should_not be_empty
-          @story.updated.should be_kind_of(Fixnum)
+          @first_story.updated_date.should_not be_empty
+          @first_story.updated.should be_kind_of(Fixnum)
+          @first_story.updated_date.should be_kind_of(String)
+          @first_story.updated_date.should == "09/22/2013 19:45:32"
         end
 
         it 'created date' do
-          @story.created_date.should_not be_empty
-          @story.created.should be_kind_of(Fixnum)
+          @first_story.created_date.should_not be_empty
+          @first_story.created.should be_kind_of(Fixnum)
+          @first_story.created_date.should be_kind_of(String)
+          @first_story.created_date.should == "09/21/2013 21:22:40"
         end
 
         it 'type' do
-          @story.type.should_not be_empty
+          @first_story.type.should_not be_empty
         end
 
         it 'creator' do
-          @story.creator.should_not be_empty
+          @first_story.creator.should_not be_empty
         end
 
         it 'size' do
-          @story.size.should_not be_empty
+          @first_story.size.should_not be_empty
         end
       end
 
       context 'has the optional fields' do
         it 'url' do
-          @story.url.should_not be_empty
+          @first_story.url.should_not be_empty
+        end
+
+        it 'owner' do
+          @first_story.owner.should_not be_nil
+          @first_story.owner.should_not be_empty
         end
       end
     end
