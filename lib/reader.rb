86c63f0e479fe719e4e2fd579e725e770fb343b4
Mike Lutton <mlutton@groupon.com>
2013-09-28 17:37:19 -0500

    Added tests for file doesnt exist and file is empty for person file

--- lib/reader.rb
+++ lib/reader.rb
@@ -3,13 +3,22 @@ require 'transformer'
 require 'story_collection'
 
 class Reader
-  def initialize story_file_name
+  def initialize story_file_name, person_file_name
     @file_name = story_file_name
+    @person_file_name = person_file_name
   end
 
   def read
     content = File.read(@file_name)
     story_data = JSON.parse(content) rescue story_data = {}
+
+    person_content = File.read(@person_file_name)
+    person_data = JSON.parse(person_content) rescue person_data = {}
+
+    if person_data.length == 0
+      raise "Invalid Person File Format"
+    end
+
     return Transformer.transform(story_data) if valid_content?(story_data)
     raise "Invalid File Format"
   end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -2,26 +2,46 @@ require 'reader'
 
 describe Reader do
   describe '#read', :focus => true do
-    it 'should raise a file not found error when file does not exist' do
-      reader = Reader.new('file_not_found.json')
-      lambda { reader.read }.should raise_error("No such file or directory - file_not_found.json")
+    let(:missing_file) { 'file_not_found.json' }
+    let(:default_person_file) {'features/support/person_data.json'}
+    let(:empty_json_file) { 'features/support/empty.json' }
+    let(:bogus_tracker_file) {'features/support/bogus_tracker.json'}
+    let(:valid_tracker_file) {'features/support/tracker_data.json'}
+
+    describe 'story file' do
+      it 'should raise a file not found error when file does not exist' do
+        reader = Reader.new(missing_file, default_person_file)
+        lambda { reader.read }.should raise_error("No such file or directory - file_not_found.json")
+      end
+
+      it 'should raise an error when file is empty' do
+        reader = Reader.new(empty_json_file, default_person_file)
+        lambda { reader.read }.should raise_error("Invalid File Format")
+      end
+
+      it 'should raise an error when file format is not correct' do
+        reader = Reader.new(bogus_tracker_file, default_person_file)
+        lambda { reader.read }.should raise_error("Invalid File Format")
+      end
+
+      it 'should have tracker stories if the file format is correct' do
+        reader = Reader.new(valid_tracker_file, default_person_file)
+        data = reader.read
+        data.story_count > 0
+      end
+
     end
 
-    it 'should raise an error when file is empty' do
-      reader = Reader.new('features/support/empty.json')
-      lambda { reader.read }.should raise_error("Invalid File Format")
-    end
+    describe 'person file' do
+      it 'should raise a file not found error when file does not exist' do
+        reader = Reader.new(valid_tracker_file, missing_file)
+        lambda { reader.read }.should raise_error("No such file or directory - file_not_found.json")
+      end
 
-    it 'should raise an error when file format is not correct' do
-      reader = Reader.new('features/support/bogus_tracker.json')
-      lambda { reader.read }.should raise_error("Invalid File Format")
+      it 'should raise an error when file is empty' do
+        reader = Reader.new(valid_tracker_file, empty_json_file)
+        lambda { reader.read }.should raise_error("Invalid Person File Format")
+      end
     end
-
-    it 'should have tracker stories if the file format is correct' do
-      reader = Reader.new('features/support/tracker_data.json')
-      data = reader.read
-      data.story_count > 0
-    end
-
   end
 end
