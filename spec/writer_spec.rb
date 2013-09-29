295cce07aed12e26fad7feabe30745b81e438442
Michael (Doc) Norton <docondev@gmail.com>
2013-09-29 09:33:25 -0500

    specifies behavior is for write

--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -4,19 +4,20 @@ NEW_FILE = 'features/support/new_file.json'
 EXISTING_FILE = 'features/support/existing_file.json'
 
 describe Writer, :focus => true do
-  it 'should create a new file when the file does not exist' do
-    File.delete(NEW_FILE) if File.exist?(NEW_FILE)
-    writer = Writer.new(NEW_FILE)
-    writer.write
-    File.exist?(NEW_FILE).should == true
-  end
+  describe '#write' do
+    it 'should create a new file when the file does not exist' do
+      File.delete(NEW_FILE) if File.exist?(NEW_FILE)
+      writer = Writer.new(NEW_FILE)
+      writer.write
+      File.exist?(NEW_FILE).should == true
+    end
 
-  it 'should append to a file when the file already exists' do
-    File.open(EXISTING_FILE, "w+") { |f| f.puts('stuff') } if !File.exists?(EXISTING_FILE)
-    start_size = File.size(EXISTING_FILE)
-    writer = Writer.new(EXISTING_FILE)
-    writer.write
-    File.size(EXISTING_FILE).should be > start_size
+    it 'should append to a file when the file already exists' do
+      File.open(EXISTING_FILE, "w+") { |f| f.puts('stuff') } if !File.exists?(EXISTING_FILE)
+      start_size = File.size(EXISTING_FILE)
+      writer = Writer.new(EXISTING_FILE)
+      writer.write
+      File.size(EXISTING_FILE).should be > start_size
+    end
   end
-
 end
