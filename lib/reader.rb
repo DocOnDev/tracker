c8e6d52c58e5a15bc635e275e5335de987d83276
Michael (Doc) Norton <docondev@gmail.com>
2013-10-22 08:05:45 -0500

    Properly handles type parameter

--- lib/reader.rb
+++ lib/reader.rb
@@ -1,7 +1,15 @@
 require 'tracker_reader'
+require 'file_reader'
 
 class Reader
-  def self.create
-    TrackerReader.new
+  def self.create type=:tracker
+    case type
+    when :tracker
+      return TrackerReader.new
+    when :file
+      return FileReader.new
+    else
+      raise "Invalid Reader Type Specified"
+    end
   end
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -4,4 +4,12 @@ describe Reader, :focus => true  do
   it "creates a tracker reader by default" do
     Reader.create.should be_kind_of(TrackerReader)
   end
+
+  it "creates a file reader when specified" do
+    Reader.create(:file).should be_kind_of(FileReader)
+  end
+
+  it "throws an error for invalid reader types" do
+    lambda{ Reader.create(:foobar) }.should raise_error("Invalid Reader Type Specified")
+  end
 end
