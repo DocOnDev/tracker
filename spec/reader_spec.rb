4a9947cc048e873590f15857fa5cbc0875e9500a
Michael (Doc) Norton <docondev@gmail.com>
2013-10-26 10:03:23 -0500

    Describes .create

--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,15 +1,21 @@
 require 'reader'
 
 describe Reader, :focus => true  do
-  it "creates a tracker reader by default" do
-    Reader.create.should be_kind_of(TrackerReader)
-  end
+  describe ".create" do
+    it "creates tracker reader by default" do
+      Reader.create.should be_kind_of(TrackerReader)
+    end
 
-  it "creates a file reader when specified" do
-    Reader.create(:file).should be_kind_of(FileReader)
-  end
+    it "creates file reader when specified" do
+      Reader.create(:file).should be_kind_of(FileReader)
+    end
 
-  it "throws an error for invalid reader types" do
-    lambda{ Reader.create(:foobar) }.should raise_error("Invalid Reader Type Specified")
+    it "creates tracker reader when specified" do
+      Reader.create(:tracker).should be_kind_of(TrackerReader)
+    end
+
+    it "throws error for invalid reader types" do
+      lambda{ Reader.create(:foobar) }.should raise_error("Invalid Reader Type Specified")
+    end
   end
 end
